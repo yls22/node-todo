cd8635111c2f843bac8829d7e9f7d5b262ccd5fb
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-06 21:00:47 +0700

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5b912ba29436bf27e0636a811';
+
+// if(!ObjectID.isValid(id)) {
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5b8b8c2aecb6bb49d8db87ab').then((user) => {
+  if(!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
