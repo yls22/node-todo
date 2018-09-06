c61118e22b7f684dd0b44b7665317e79ecc07263
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-06 19:31:47 +0700

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp',{useNewUrlParser: true});
 
 module.exports =  {mongoose};
