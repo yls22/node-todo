f34693559d3829fc2540fc3b50144b2008e53a59
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-09 18:39:49 +0700

    Update app Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://yls22:123456@ds149672.mlab.com:49672/todo',{useNewUrlParser: true});
+mongoose.connect('mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo',{useNewUrlParser: true});
 
 module.exports =  {mongoose};
