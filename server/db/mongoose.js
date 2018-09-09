f8ff9ae7bcc09bbda1b56de5b701ba90b177894e
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-09 18:12:05 +0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<yls22>:<123456>@ds149672.mlab.com:49672/todo',{useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://<yls22>:<123456>@ds149672.mlab.com:49672/todo',{useNewUrlParser: true});
 
 module.exports =  {mongoose};
