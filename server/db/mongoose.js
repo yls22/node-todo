5546c663b1b2d2166921e3bc81a81b2fa7b813d0
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-11 15:23:24 +0700

    Heroku app

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo?', {useNewUrlParser: true, authMechanism: 'ScramSHA1'});
+mongoose.connect('mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo?', {useNewUrlParser: true});
 
 module.exports =  {mongoose};
