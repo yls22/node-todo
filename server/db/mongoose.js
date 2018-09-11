2717622721a97834240ed6323b018cd33d247d22
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-11 15:35:40 +0700

    Heroku app

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo?', {useNewUrlParser: true});
+mongoose.connect('mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo?' || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports =  {mongoose};
+
+//process.env.MONGODB_URI || 
