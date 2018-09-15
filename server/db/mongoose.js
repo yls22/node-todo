97b25121b934568c907237e409a23a7bd8e2cd6e
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-15 17:41:08 +0700

    Add PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 //var mongoDB = 'mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo';
-
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
