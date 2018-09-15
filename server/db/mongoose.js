f9dc259a7084684eeefae91b74c5164077fdf900
Yulius Susantoputra <yulius.susantoputra@gmail.com>
2018-09-16 03:59:57 +0700

    Setup test and dev env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,6 @@ var mongoose = require('mongoose');
 //var mongoDB = 'mongodb://yls22:orang2289@ds149672.mlab.com:49672/todo';
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect( process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports =  {mongoose};
-
-//process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
